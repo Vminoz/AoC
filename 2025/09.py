53ba9ece4e0ba25360f376ca86c222d88dcb923b
Vminoz <minoz.valentin@gmail.com>
2025-12-17 13:07:41 +0100

    25-09:speedup

--- 2025/09.py
+++ 2025/09.py
@@ -16,7 +16,6 @@ CLOCKWISE: dict[P2D, P2D] = {
     (1, 0): (0, -1),
     (0, -1): (-1, 0),
 }
-
 COUNTERCLOCKWISE = {v: k for k, v in CLOCKWISE.items()}
 
 
@@ -24,7 +23,9 @@ class RectPolygon:
     """Ordered polygon with orthogonal edges"""
 
     def __init__(self, verts: Iterable[P2D]) -> None:
-        self.verts = [*verts]
+        self.verts = list(verts)
+        if len(self.verts) % 2:
+            raise ValueError("Cannot make a RectPolygon with uneven number of verts")
         # Ensure polygon is counterclockwise
         if not is_counterclockwise(self.verts):
             self.verts.reverse()
@@ -38,6 +39,10 @@ class RectPolygon:
                 break
 
     def __contains__(self, other: "RectPolygon") -> bool:
+        if len(other.verts) < 5:
+            # If other is a rectangle (4 or 2 vertices), use optimized check
+            return self._contains_rect(other)
+
         for o_edge in other.edges():
             oe = EdgeInfo(o_edge)
             for self_edge in self.edges():
@@ -65,6 +70,46 @@ class RectPolygon:
                 ):
                     return False
         return True
+
+    def _contains_rect(self, rect: "RectPolygon") -> bool:
+        """Optimized contains check when 'other' is known to be a rectangle"""
+        i_all = [v[0] for v in rect.verts]
+        j_all = [v[1] for v in rect.verts]
+        rect_range = [[min(i_all), max(i_all)], [min(j_all), max(j_all)]]
+
+        rect_center = (
+            rect_range[0][0] + (rect_range[0][1] - rect_range[0][0]) // 2,
+            rect_range[1][0] + (rect_range[1][1] - rect_range[1][0]) // 2,
+        )
+        is_inside = False
+
+        for p1, p2 in self.edges():
+            fd = int(p1[1] == p2[1])
+            fixed_val = p1[fd]
+            fixed_range = rect_range[fd]
+            cd = fd ^ 1
+            change_range = rect_range[cd]
+            change_val = sorted((p1[cd], p2[cd]))
+
+            if (
+                # Sort of raycast
+                fd == 0  # horizontal
+                and fixed_val < rect_center[0]  # above rect
+                and change_val[0] <= rect_center[1] < change_val[1]  # aligned
+            ):
+                # if rect is inside, it should have an uneven number of edges above its center point
+                is_inside = not is_inside
+
+            if not fixed_range[0] < fixed_val < fixed_range[1]:
+                continue  # edge is somewhere else - ok
+            # edge is  perpendicular and: [│ │ ─] or [│ ┼] or [│─│]
+
+            if max(change_val[0], change_range[0]) < min(
+                change_val[1], change_range[1]
+            ):
+                #  [│ ┼] or [│─│]
+                return False
+        return is_inside
 
     def _log_edge_check(self, edges, o_edge, self_edge):
         if len(self.verts) > 100:
@@ -116,10 +161,6 @@ def points_on_edge(edge: Edge) -> Generator[P2D]:
             yield i, j
 
 
-def edge_length(edge: Edge) -> int:
-    return 1 + abs(edge[0][0] - edge[1][0]) + abs(edge[0][1] - edge[1][1])
-
-
 def edge_direction(edge):
     di = edge[1][0] - edge[0][0]
     dj = edge[1][1] - edge[0][1]
@@ -130,6 +171,52 @@ def edge_direction(edge):
 
 def rect_area(a: P2D, b: P2D) -> int:
     return (abs(a[0] - b[0]) + 1) * (abs(a[1] - b[1]) + 1)
+
+
+def calc_largest_inner_rectangle_area(tiles: list[P2D]) -> int:
+    bounds = RectPolygon(tiles)
+    is_bounds_large = len(bounds.verts) > 100
+    largest_rect = None
+    largest_area = -1
+    for i, t1 in enumerate(tiles):
+        for t2 in tiles[i + 1 :]:
+            area = rect_area(t1, t2)
+            if area <= largest_area:
+                continue
+            rect = RectPolygon.from_rect_corners(t1, t2)
+            rect_is_in_polygon = rect in bounds
+
+            if rect_is_in_polygon:
+                largest_rect = rect
+                largest_area = area
+
+            if logger.is_verbose:
+                if not is_bounds_large:
+                    symbol = "O" if rect_is_in_polygon else "X"
+                    logger.m(
+                        p2d_sets_string(
+                            set(rect.verts),
+                            {p for e in bounds.edges() for p in points_on_edge(e)},
+                            symbols={t1: symbol, t2: symbol},
+                        )
+                        + str(area)
+                    )
+                elif rect_is_in_polygon:
+                    logger.v(area, t1, t2)
+
+    if largest_rect is None:
+        raise ValueError(f"Found no rectangles for {tiles=}")
+
+    if is_bounds_large:
+        rect = largest_rect
+        make_polygon_svg(
+            bounds.verts,
+            rect.verts,
+            rect.verts[:-1] if rect.verts[0] in [t1, t2] else rect.verts[1:],
+            file=Path(__file__).parent / "vis" / "09.svg",
+        )
+
+    return largest_area
 
 
 def parse_tiles(filename: str) -> list[P2D]:
@@ -146,41 +233,7 @@ def largest_rectangle(filename: str) -> int:
 
 def largest_bounded_rectangle(filename: str) -> int:
     tiles = parse_tiles(filename)
-    bounds = RectPolygon(tiles)
-    is_bounds_large = len(bounds.verts) > 100
-    largest = (RectPolygon([(0, 0)]), -1)
-    for i, t1 in enumerate(tiles):
-        for t2 in tiles[i + 1 :]:
-            area = rect_area(t1, t2)
-            if area <= largest[1]:
-                continue
-            rect = RectPolygon.from_rect_corners(t1, t2)
-            rect_is_in_polygon = rect in bounds
-            if logger.is_verbose:
-                if not is_bounds_large:
-                    symbol = "O" if rect_is_in_polygon else "X"
-                    logger.m(
-                        p2d_sets_string(
-                            set(rect.verts),
-                            {p for e in bounds.edges() for p in points_on_edge(e)},
-                            symbols={t1: symbol, t2: symbol},
-                        )
-                        + str(area)
-                    )
-                elif rect_is_in_polygon:
-                    logger.v(area, t1, t2)
-            if rect_is_in_polygon:
-                largest = rect, area
-    if is_bounds_large:
-        rect = largest[0]
-        make_polygon_svg(
-            bounds.verts,
-            rect.verts,
-            rect.verts[:-1] if rect.verts[0] in [t1, t2] else rect.verts[1:],
-            file=Path(__file__).parent / "vis" / "09.svg",
-        )
-
-    return largest[1]
+    return calc_largest_inner_rectangle_area(tiles)
 
 
 def main():
